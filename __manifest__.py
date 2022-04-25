eaaaec2ac485e4bbadd7884edc4636029ef57954
enzapps <snippebucket@gmail.com>
2022-04-25 14:48:50 +0530

    <yt>

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': "Excellance Sale Order Updation",
+    'author':
+        'ENZAPPS',
+    'summary': """
+This module is for Excellance Sale Order Updation.
+""",
+
+    'description': """
+        This module is for Excellance Sale Order Updation.
+    """,
+    'website': "",
+    'category': 'base',
+    'version': '14.0',
+    'depends': ['base', 'account', 'stock', 'product', 'sale_management', 'purchase', 'crm', 'contacts', 'hr',
+                'hr_contract', 'hr_timesheet', 'project', 'hr_expense', 'analytic', 'hr_recruitment', 'om_hr_payroll',
+                'enz_manpower','enz_manpower_overtime','om_hr_payroll_account','excellance','alhokair_einvoice_reports',
+                'excellence_einvoice','excellence_invoice_updation','excellence_corrections','excellance_sale_updation','excellance_hr_updation','excellence_new_structure'],
+    "images": ['static/description/icon.png'],
+    'data': [
+
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+}
